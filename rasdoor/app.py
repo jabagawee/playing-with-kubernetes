ebbc9881be89e1d2caec49cb15f73c396455fc39
Andrew Szeto <andrew@jabagawee.com>
2017-10-29 17:03:47 -0700

    Added a privacy policy for Rasdoor

--- rasdoor/app.py
+++ rasdoor/app.py
@@ -8,9 +8,19 @@ app = Flask(__name__)
 FACEBOOK_PAGE_ACCESS_TOKEN = os.getenv('FACEBOOK_PAGE_ACCESS_TOKEN')
 FACEBOOK_VERIFICATION_TOKEN = os.getenv('FACEBOOK_VERIFICATION_TOKEN')
 
+FACEBOOK_AUTHORIZED_SENDER_IDS = set(
+    '1552580134821661')
+
 @app.route('/')
 def hello_world():
     return 'Hello World'
+
+@app.route('/privacy')
+def privacy_policy():
+    return 'You should not be using this site if you don\'t know Szeto. ' \
+        'If you do, the privacy policy is that there is a log of who ' \
+        'opens this door. This is a private residence, so we want to ' \
+        'know who unlocks and locks our door.'
 
 @app.route('/webhook/facebook_messenger', methods=['GET', 'POST'])
 def facebook_webhook():
@@ -18,15 +28,16 @@ def facebook_webhook():
         body = request.get_json()
         if body['object'] == 'page':
             for entry in body['entry']:
+                # facebook docs say that one entry will always have only one message
                 message = entry['messaging'][0]
-                if message['sender']['id'] == '1552580134821661':
+                if message['sender']['id'] in FACEBOOK_AUTHORIZED_SENDER_IDS:
                     if message['message']['text'] == 'lock':
-                        print('Locking the August lock!')
-                        os.system('ssh -o StrictHostKeyChecking=no -i /sshkeys/mountedpi/ssh-privatekey pi@mountedpi "python3 august.py lock"')
+                        lock_august()
                     elif message['message']['text'] == 'unlock':
-                        print('Unlocking the August lock!')
-                        os.system('ssh -o StrictHostKeyChecking=no -i /sshkeys/mountedpi/ssh-privatekey pi@mountedpi "python3 august.py unlock"')
-                print(entry['messaging'][0])
+                        unlock_august()
+                    elif message['message']['text'] == 'front':
+                        unlock_front()
+                print(message)
             return 'EVENT_RECEIVED'
         abort(404)
     else:
@@ -36,6 +47,18 @@ def facebook_webhook():
         if mode == 'subscribe' and token == FACEBOOK_VERIFICATION_TOKEN:
             return challenge
         abort(403)
+
+def _run_command_on_pi(cmd):
+    os.system(f'ssh -o StrictHostKeyChecking=no -i /sshkeys/mountedpi/ssh-privatekey pi@mountedpi "{cmd}"')
+
+def lock_august():
+    _run_command_on_pi('python3 august.py lock')
+
+def unlock_august():
+    _run_command_on_pi('python3 august.py unlock')
+
+def unlock_front():
+    _run_command_on_pi('python3 opendoor.py')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
